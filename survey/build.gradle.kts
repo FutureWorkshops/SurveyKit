a2fdde89750cf9d3d7d8dfa0ca10bc81bc25b8fb
Manuel Perera <mapm14@gmail.com>
2020-06-25 12:20:03 +0200

    Add maps library, and location permission to manifest

--- survey/build.gradle.kts
+++ survey/build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation("androidx.recyclerview:recyclerview:1.0.0")
     implementation("com.airbnb.android:lottie:3.0.7")
+    api("com.google.android.gms:play-services-maps:17.0.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-engine:5.5.2")
     testImplementation("org.junit.platform:junit-platform-runner:1.5.2")
